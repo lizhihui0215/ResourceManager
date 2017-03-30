703ec14b3364c7b95a944c6bd74cbc5b7f1fb6fb
lizhihui0215@gmail.com <dsn4cgwyA>
2017-03-30 18:39:32 +0800

    refine code

--- ResourceManager/Views/RMLinkSearchViewController.swift
+++ ResourceManager/Views/RMLinkSearchViewController.swift
@@ -8,34 +8,51 @@
 
 import UIKit
 
-
-class RMLinkSearchViewController: RMViewController {
+class RMLinkCell: UITableViewCell {
     
     @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var titleLabel: UILabel!
+}
+
+
+class RMLinkSearchViewController: RMViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var linkStackView: UIStackView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.rowHeight = UITableViewAutomaticDimension
         
-
+        tableView.estimatedRowHeight = 40
     }
     @IBAction func searchButtonPressed(_ sender: UIButton) {
         let linkNode = RMLinkNodeView()
 
-        linkStackView.addArrangedSubview(linkNode)
+//        linkStackView.addArrangedSubview(linkNode)
     }
     @IBAction func cancelButtonPressed(_ sender: Any) {
-        let view = linkStackView.arrangedSubviews[0]
-        linkStackView.removeArrangedSubview(view)
+//        let view = linkStackView.arrangedSubviews[0]
+//        linkStackView.removeArrangedSubview(view)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RMLinkCell
+        cell.titleLabel.text =  ["阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿", "阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿", "阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿阿斯顿"][indexPath.row]
+        return cell
+    }
+    
     
 
     /*
